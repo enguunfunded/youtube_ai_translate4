0a3cebc08c78e963879adc60891b8aa8570c1ee9
enguunfunded <jagaa_tsg@yahoo.com>
2025-07-19 17:42:26 +0800

    Update merge_video.py

--- merge_video.py
+++ merge_video.py
@@ -1,7 +1,7 @@
 import ffmpeg
 import os
 
-def merge_all(video_path="temp/input.mp4", audio_path="temp/voice.mp3", subtitle_path="temp/subtitles.srt", output_path="output/final_video.mp4"):
+def merge_all(video_path="temp/input.mp4", audio_path="temp/voice_bark.wav", subtitle_path="temp/subtitles.srt", output_path="output/final_video.mp4"):
     ffmpeg.input(video_path).output("temp/video_no_audio.mp4", an=None).run(overwrite_output=True)
     
     ffmpeg.output(
