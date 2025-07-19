55dd71647327ea7156d3fddc9acc539de682f825
enguunfunded <jagaa_tsg@yahoo.com>
2025-07-19 17:25:30 +0800

    Update voiceover.py

--- voiceover.py
+++ voiceover.py
@@ -1,5 +1,5 @@
 from gtts import gTTS
 
-def generate_voice(text, output_path="temp/voice.mp3", lang='mn'):
+def generate_voice(text, output_path="temp/voice.mp3", lang='en'):
     tts = gTTS(text=text, lang=lang)
     tts.save(output_path)
